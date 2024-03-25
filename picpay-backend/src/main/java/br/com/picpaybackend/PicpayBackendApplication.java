21f0d871419b39a9ca2c580e835cbfb35367c85b
Ace0777 <luishhasantos@gmail.com>
2024-03-25 15:57:16 -0300

    Configurando projeto e estruturando dominios do mesmo

---
+++ picpay-backend/src/main/java/br/com/picpaybackend/PicpayBackendApplication.java
@@ -0,0 +1,13 @@
+package main.java.br.com.picpaybackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PicpayBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PicpayBackendApplication.class, args);
+	}
+
+}
